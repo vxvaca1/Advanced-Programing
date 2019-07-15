5d65ce23e46ae21d7102b7af3d678e117ba3e9e9
Vicente Xavier Vaca Perez <32905919+vxvaca1@users.noreply.github.com>
2019-07-15 13:45:53 -0500

    vxvaca1

---
+++ Labs/Lab9/login_files/formoid1/form.php
@@ -0,0 +1,30 @@
+<?php
+
+define('EMAIL_FOR_REPORTS', 'ferchoes_16@hotmail.com');
+define('RECAPTCHA_PRIVATE_KEY', '@privatekey@');
+define('FINISH_URI', 'http://');
+define('FINISH_ACTION', 'message');
+define('FINISH_MESSAGE', 'Thanks for filling out my form!');
+define('UPLOAD_ALLOWED_FILE_TYPES', 'doc, docx, xls, csv, txt, rtf, html, zip, jpg, jpeg, png, gif');
+
+define('_DIR_', str_replace('\\', '/', dirname(__FILE__)) . '/');
+require_once _DIR_ . '/handler.php';
+
+?>
+
+<?php if (frmd_message()): ?>
+<link rel="stylesheet" href="<?php echo dirname($form_path); ?>/formoid-biz-red.css" type="text/css" />
+<span class="alert alert-success"><?php echo FINISH_MESSAGE; ?></span>
+<?php else: ?>
+<!-- Start Formoid form-->
+<link rel="stylesheet" href="<?php echo dirname($form_path); ?>/formoid-biz-red.css" type="text/css" />
+<script type="text/javascript" src="<?php echo dirname($form_path); ?>/jquery.min.js"></script>
+<form class="formoid-biz-red" style="background-color:#1A2223;font-size:14px;font-family:'Open Sans','Helvetica Neue', 'Helvetica', Arial, Verdana, sans-serif;color:#ECECEC;max-width:480px;min-width:150px" method="post"><div class="title"><h2>Login</h2></div>
+	<div class="element-input<?php frmd_add_class("input"); ?>" title="userName"><label class="title"></label><input class="large" type="text" name="input" placeholder="userName"/></div>
+	<div class="element-password<?php frmd_add_class("password"); ?>" title="password"><label class="title"></label><input class="large" type="password" name="password" value="" placeholder="password"/></div>
+<div class="submit"><input type="submit" value="Submit"/></div></form><script type="text/javascript" src="<?php echo dirname($form_path); ?>/formoid-biz-red.js"></script>
+
+<!-- Stop Formoid form-->
+<?php endif; ?>
+
+<?php frmd_end_form(); ?>
